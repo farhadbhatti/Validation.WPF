165e8f2ad26339a85ba892ee091b0e0a617d6043
farhadbhatti <farhadakram@outlook.com>
2017-11-27 15:45:29 +0500

    Validation Library Created

---
+++ CGI.Validation.WPF/MessageEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CGI.Validation.WPF
+{
+    public class MessageEventArgs : EventArgs
+    {
+        public MessageEventArgs()
+        {
+            this.Message = string.Empty;
+            this.Caption = string.Empty;
+        }
+
+        public string Message { get; set; }
+
+        public string Caption { get; set; }
+    }
+}
---
+++ CGI.Validation.WPF/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CGI.Validation.WPF")]
+[assembly: AssemblyDescription("This Assembly can be used to perform WPF Validations with Custom RUles")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CodeGenius Inc.")]
+[assembly: AssemblyProduct("CGI.Validation.WPF")]
+[assembly: AssemblyCopyright("Copyright © codegenius inc.  2017")]
+[assembly: AssemblyTrademark("cgi.validation.wpf")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1c2a8970-1ccc-4f2d-8d5d-f16e1df75db5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
